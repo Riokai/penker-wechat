e84528521243e38a4c494f47f85072118d47cd53
Kai <zengkai2009@foxmail.com>
2015-12-11 09:26:52 +0800

    add utils module

---
+++ app/js/lib/utils.js
@@ -0,0 +1,22 @@
+define(function() {
+
+  var obj = {
+    getObjectURL: function(file) {
+
+      var url = null;
+
+      if (window.createObjectURL != undefined) { // basic
+        url = window.createObjectURL(file);
+      } else if (window.URL != undefined) { // mozilla(firefox)
+        url = window.URL.createObjectURL(file);
+      } else if (window.webkitURL != undefined) { // webkit or chrome
+        url = window.webkitURL.createObjectURL(file);
+      }
+      
+      return url;
+    }
+  };
+
+  return obj;
+
+});
